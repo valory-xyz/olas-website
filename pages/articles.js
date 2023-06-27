3232e986cdd523ca1e54a35e62b0bd7b4365a48f
Oaksprout <oaksproutthetan@gmail.com>
2023-06-27 12:54:51 +0200

    rollback meta change

--- pages/articles.js
+++ pages/articles.js
@@ -1,11 +1,9 @@
 import Articles from "@/components/Content/Articles";
 import PageWrapper from "@/components/Layout/PageWrapper";
 import SectionWrapper from "@/components/Layout/SectionWrapper";
-import Meta from "@/components/Meta";
 
 const ArticlesPage = () =>
   <PageWrapper>
-    <Meta pageTitle="Articles" />
     <SectionWrapper backgroundType={"SUBTLE_GRADIENT"}>
       <Articles />
     </SectionWrapper>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Header from '@/components/Layout/Header'
 import Hero from '@/components/HomepageSection/Hero'
 import ForDAOs from '@/components/HomepageSection/ForDAOs'
 import ForDevs from '@/components/HomepageSection/ForDevs'
@@ -9,20 +7,16 @@ import OlasUtility from '@/components/HomepageSection/OlasUtility'
 import Framework from '@/components/HomepageSection/Framework'
 import Services from '@/components/HomepageSection/Services'
 import AppShowcase from '@/components/HomepageSection/AppShowcase'
-import Footer from '@/components/Layout/Footer'
 import Content from '@/components/HomepageSection/Content'
 import Friends from '@/components/HomepageSection/Friends'
 import Contribute from '@/components/HomepageSection/Contribute'
 import PageWrapper from '@/components/Layout/PageWrapper'
-import Head from 'next/head'
-import Meta from '@/components/Meta'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <Meta />
       <PageWrapper>
         <Hero />
         <ForDAOs />
