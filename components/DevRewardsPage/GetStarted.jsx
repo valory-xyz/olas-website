15100b04de458c382b299a9431efc54cdfad1d3b
Oaksprout <oaksproutthetan@gmail.com>
2023-08-07 09:16:17 +0100

    incorporate feedbacl

--- components/DevRewardsPage/GetStarted.jsx
+++ components/DevRewardsPage/GetStarted.jsx
@@ -10,13 +10,11 @@ function GetStarted() {
         <div class="lg:col-span-6 px-5 lg:p-0 mb-12">
           <SectionHeading>Start earning rewards</SectionHeading>
           <ol className="text-xl text-slate-800 list-decimal mb-6 pl-5 leading-loose">
-            <li>Develop a service* using <a href='https://docs.autonolas.network/open-autonomy' className="text-primary">Open Autonomy</a></li>
-            <li>Mint your service on <a href='https://registry.olas.network/services/mint' className="text-primary">Olas Protocol Registry</a></li>
-            <li><a href="https://member.olas.network">Lock</a> 10k OLAS</li>
-            <li>Get rewards in ETH & OLAS**</li>
+            <li>Write code using <a href='https://docs.autonolas.network/open-autonomy' className="text-primary">Open Autonomy</a></li>
+            <li>Mint your code on <a href='https://registry.olas.network/services/mint' className="text-primary">Olas Protocol Registry</a></li>
+            <li>Get rewards in ETH & OLAS*</li>
           </ol>
-          <div className="mb-4 text-slate-500">*You can also just develop units of code – agents and components – rather than full services. But it is recommended to develop full services at first. Learn more in the section below.</div>
-          <div className="text-slate-500">**This requires that somebody donates to your service.</div>
+          <div className="text-slate-500">*Requires that somebody with at least 10k veOLAS donates to your service.</div>
         </div>
         <div className="lg:mt-0 lg:col-span-6 lg:flex">
           <Image
