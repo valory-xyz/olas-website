0cb994432b1d1c1aeed7cb299cce11d854a15e75
Oaksprout <66292936+oaksprout@users.noreply.github.com>
2024-01-25 18:41:58 +0000

    Update link in Header component

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -18,12 +18,12 @@ const Header = () => {
             />
           </Link>
           <div class="flex items-center lg:order-2">
-            <a
-              href="#get-involved"
+            <Link
+              href="/#get-involved"
               class="text-purple-950 border border-purple-950  hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2  focus:outline-none "
             >
               Get involved
-            </a>
+            </Link>
           </div>
           <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
             <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
