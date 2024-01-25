2577dcb5b4c7daf4264631cb0c5dbb9d1ad006d1
Oaksprout <66292936+oaksprout@users.noreply.github.com>
2024-01-25 18:42:31 +0000

    Update TITLE style in globals.js

--- styles/globals.js
+++ styles/globals.js
@@ -1,3 +1,6 @@
 export const TEXT = "text-xl font-light text-gray-600"
-export const TITLE = "text-6xl text-gray-700 font-manrope tracking-tight lg:text-heading mb-12"
+export const TITLE = {
+    BIG: "text-6xl text-gray-700 font-manrope tracking-tight lg:text-heading mb-12",
+    SMALL: "text-5xl font-bold text-gray-800 tracking-tight leading-normal",
+}
 export const BUTTON = "inline-flex bg-purple-900 text-white items-center justify-center px-6 py-4 text-xl sm:text-3xl lg:text-xl sm:px-8 sm:py-5 text-center border border-purple-900 rounded-lg hover:bg-purple-950 hover:bg-repeat hover:bg-size-50 focus:ring-4 focus:ring-gray-100  lg:px-6 lg:py-4"
