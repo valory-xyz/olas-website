bc6302cca8d546c265b7ea177dc972676990f0bf
Josh Miller <mlxndrj@gmail.com>
2024-02-21 13:45:35 +0000

    feat: add build page

---
+++ pages/build.js
@@ -0,0 +1,12 @@
+import PageWrapper from '@/components/Layout/PageWrapper';
+import Meta from '@/components/Meta';
+import Build from '@/components/BuildPage';
+
+const DevRewardsPage = () => (
+  <PageWrapper>
+    <Meta pageTitle="Olas Build" />
+    <Build />
+  </PageWrapper>
+);
+
+export default DevRewardsPage;
