443b2a159b33020b67a458530ebfdb8af97f50a3
Josh Miller <mlxndrj@gmail.com>
2024-02-21 13:46:37 +0000

    fix: remove unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,16 @@
-import { Inter } from 'next/font/google'
-import Hero from '@/components/HomepageSection/Hero'
-import ForDAOs from '@/components/HomepageSection/ForDAOs'
-import ForDevs from '@/components/HomepageSection/ForDevs'
-import Flywheel from '@/components/HomepageSection/Flywheel'
-import OlasUtility from '@/components/HomepageSection/OlasUtility'
-import Framework from '@/components/HomepageSection/Framework'
-import Services from '@/components/HomepageSection/Services'
-import AppShowcase from '@/components/HomepageSection/AppShowcase'
-import Content from '@/components/HomepageSection/Content'
-import Friends from '@/components/HomepageSection/Friends'
-import Contribute from '@/components/HomepageSection/Contribute'
-import PageWrapper from '@/components/Layout/PageWrapper'
-import Meta from '@/components/Meta'
-import Affordances from '@/components/HomepageSection/Affordances'
-
-const inter = Inter({ subsets: ['latin'] })
+import Hero from '@/components/HomepageSection/Hero';
+import ForDAOs from '@/components/HomepageSection/ForDAOs';
+import ForDevs from '@/components/HomepageSection/ForDevs';
+import Flywheel from '@/components/HomepageSection/Flywheel';
+import Framework from '@/components/HomepageSection/Framework';
+import Services from '@/components/HomepageSection/Services';
+import AppShowcase from '@/components/HomepageSection/AppShowcase';
+import Content from '@/components/HomepageSection/Content';
+import Friends from '@/components/HomepageSection/Friends';
+import Contribute from '@/components/HomepageSection/Contribute';
+import PageWrapper from '@/components/Layout/PageWrapper';
+import Meta from '@/components/Meta';
+import Affordances from '@/components/HomepageSection/Affordances';
 
 export default function Home() {
   return (
@@ -32,5 +28,5 @@ export default function Home() {
       <Friends />
       <Contribute />
     </PageWrapper>
-  )
+  );
 }
