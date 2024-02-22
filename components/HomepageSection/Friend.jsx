927748412ec2685746f8820532fd65c339fb2fdc
Josh Miller <mlxndrj@gmail.com>
2024-02-22 19:54:27 +0000

    fix: Refactor Friend component styling

--- components/HomepageSection/Friend.jsx
+++ components/HomepageSection/Friend.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Friend = ({ friend }) => {
   return (
-      <a href={friend.url} className='image-container grayscale' style={{ height: 50 }}>        
+      <a href={friend.url} className='h-[50px] w-full grayscale' target="_blank">        
           <Image src={`/images/friends/${friend.imageFilename}`} alt={friend.name} width={150} height={100}  className="mx-auto" />
       </a>
   );
