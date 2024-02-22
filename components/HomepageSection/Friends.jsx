c6fbef70b351dbd57225ca5c234751814feac590
Josh Miller <mlxndrj@gmail.com>
2024-02-22 18:14:47 +0000

    fix: move sortedFriends outside component, add id friends

--- components/HomepageSection/Friends.jsx
+++ components/HomepageSection/Friends.jsx
@@ -3,11 +3,12 @@ import friends from "@/data/friends.json";
 import SectionHeading from "../SectionHeading";
 import SectionWrapper from "../Layout/SectionWrapper";
 
+
+const sortedFriends = friends.sort((a, b) => a.name.localeCompare(b.name));
 const Friends = () => {
-  const sortedFriends = friends.sort((a, b) => a.name.localeCompare(b.name));
 
   return (
-    <SectionWrapper>
+    <SectionWrapper id="friends">
     <section class="bg-white max-w-[1000px] mx-auto md:p-12">
       <div class="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
         <div className="text-center mb-12">
