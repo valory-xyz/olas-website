581ff62f3c9042eb94802174131341bcd7bbe31a
Josh Miller <mlxndrj@gmail.com>
2024-02-22 18:14:00 +0000

    fix: image stretching

--- components/HomepageSection/Friend.jsx
+++ components/HomepageSection/Friend.jsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Friend = ({ friend }) => {
   return (
-      <a href={friend.url} className='relative'>
-        <Image src={`/images/friends/${friend.imageFilename}`} alt={friend.name} width={150} height={100}  className="mx-auto max-h-[100px]" />
+      <a href={friend.url} className='image-container' style={{ height: 50 }}>        
+          <Image src={`/images/friends/${friend.imageFilename}`} alt={friend.name} width={150} height={100}  className="mx-auto" />
       </a>
   );
 }
