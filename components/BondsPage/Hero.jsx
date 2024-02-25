b33180414cebde97b06a6d97bfa44ece3744ca02
Oaksprout <66292936+oaksprout@users.noreply.github.com>
2024-02-24 18:08:45 -0700

    Update bond CTA buttons

--- components/BondsPage/Hero.jsx
+++ components/BondsPage/Hero.jsx
@@ -16,11 +16,19 @@ const Hero = () => {
           </SectionHeading>
           <a
             href={CTA}
-            class="inline-flex bg-purple-900 text-white items-center justify-center px-6 py-4 text-xl sm:text-3xl lg:text-xl sm:px-8 sm:py-5 text-center border border-primary rounded-lg hover:bg-dark-hexagons1 hover:bg-repeat hover:bg-size-50 focus:ring-4 focus:ring-gray-100  lg:px-6 lg:py-4"
+            class="inline-flex bg-purple-900 text-white items-center justify-center px-6 py-4 text-xl sm:text-3xl lg:text-xl sm:px-8 sm:py-5 text-center border border-primary rounded-lg hover:bg-dark-hexagons1 hover:bg-repeat hover:bg-size-50 focus:ring-4 focus:ring-gray-100  lg:px-6 lg:py-4 lg:mr-4 sm:mb-4"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Bond now
+            Explore bond paths
+          </a>
+          <a
+            href={CTA}
+            class="inline-flex bg-white text-purple-900 items-center justify-center px-6 py-4 text-xl sm:text-3xl lg:text-xl sm:px-8 sm:py-5 text-center border border-primary rounded-lg hover:bg-gray-100 hover:bg-repeat hover:bg-size-50 focus:ring-4 focus:ring-gray-100  lg:px-6 lg:py-4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View bonding products
           </a>
         </div>
         <div className="lg:mt-0 lg:col-span-6 lg:flex">
