2ad0486de2b65fd06b23d73ea28add1c2131eedb
mohandast52 <mohandast52@gmail.com>
2024-04-02 15:00:33 +0530

    sort videos and podcasts

--- components/Content/Videos.jsx
+++ components/Content/Videos.jsx
@@ -3,34 +3,43 @@ import Link from 'next/link';
 import videos from 'data/videos.json';
 import Video from './Video';
 
-const Videos = ({ limit }) => (
-  <section>
-    <div>
+const Videos = ({ limit }) => {
+  const videosSortedByDate = videos.sort(
+    (a, b) => new Date(b.date) - new Date(a.date),
+  );
+
+  return (
+    <section>
       <div>
-        <h2 className="mb-4 text-3xl md:text-5xl lg:text-4xl tracking-tight font-extrabold text-gray-900 ">
-          Videos & Podcasts
-        </h2>
-        {limit !== null && (
-        <div className="mb-4">
-          <Link
-            href="/videos"
-            className="text-xl md:text-2xl text-primary hover:text-primary-800 transition-colors duration-300"
-          >
-            See all ▶
-          </Link>
+        <div>
+          <h2 className="mb-4 text-3xl md:text-5xl lg:text-4xl tracking-tight font-extrabold text-gray-900 ">
+            Videos & Podcasts
+          </h2>
+          {limit !== null && (
+            <div className="mb-4">
+              <Link
+                href="/videos"
+                className="text-xl md:text-2xl text-primary hover:text-primary-800 transition-colors duration-300"
+              >
+                See all ▶
+              </Link>
+            </div>
+          )}
         </div>
-        )}
+        <div className="grid gap-8 lg:grid-cols-3">
+          {(limit
+            ? videosSortedByDate.slice(0, limit)
+            : videosSortedByDate
+          ).map((video) => (
+            <div key={video.platform_link}>
+              <Video video={video} />
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="grid gap-8 lg:grid-cols-3">
-        {(limit ? videos.slice(0, limit) : videos).map((video) => (
-          <div key={video.platform_link}>
-            <Video video={video} />
-          </div>
-        ))}
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 Videos.propTypes = { limit: PropTypes.number };
 Videos.defaultProps = { limit: null };
