e043e17ebc68f98b59d421341b2621afb918b4ae
Josh Miller <mlxndrj@gmail.com>
2024-04-18 16:21:01 +0100

    Update header and menu styles in components/Layout folder

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -7,7 +7,7 @@ import { Menu } from './Menu';
 const Header = () => (
   <header className="sticky top-0 z-50 border-b">
     <nav className="bg-white px-4 lg:px-6 py-2.5 ">
-      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl ">
+      <div className="flex flex-wrap justify-start items-center mx-auto max-w-screen-xl gap-10">
         <Link href="/">
           <Image
             src="/images/olas-logo.svg"
