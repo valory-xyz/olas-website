3e64e457efec7bd6e1be30ddd22b32131f58d9d0
Atatakai <atatakainyan@gmail.com>
2024-04-24 20:13:53 +0400

    Add explore page

--- components/LearnPage/index.jsx
+++ components/LearnPage/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import SectionWrapper from 'components/Layout/SectionWrapper';
 import WhatIsOlas from './2WhatIsOlas';
 import DecentralizedAndSophisticated from './3DecentralizedAndSophisticated';
 import HowDoOlasWork from './4HowDoOlasWork';
@@ -23,10 +24,10 @@ export const LEARN_LIST = [
 
 const LearnPage = () => (
   <PageWrapper>
-    <Meta title="Learn" description="Build your understanding of what Olas is and how it works." />
-    <section className="bg-white mb-10 container px-4 mt-4">
-      <h2 className="text-4xl font-bold tracking-tighter text-center sm:text-5xl md:text-6xl mb-6">Learn</h2>
-      <h4 className="text-2xl font-semibold text-center mb-8">Jump To:</h4>
+    <Meta pageTitle="Learn" description="Build your understanding of what Olas is and how it works." />
+    <SectionWrapper customClasses="mt-12 mb-20 container">
+      <h1 className="text-4xl font-bold tracking-tighter text-center sm:text-5xl md:text-6xl mb-6">Learn</h1>
+      <span className="block text-2xl text-center mb-8">Jump To:</span>
       <div className="bg-gray-100 rounded-lg p-6 mx-auto">
         <ul>
           {LEARN_LIST.map((e) => (
@@ -38,7 +39,7 @@ const LearnPage = () => (
           ))}
         </ul>
       </div>
-    </section>
+    </SectionWrapper>
 
     <div className="container mx-auto px-4">
       <WhatIsOlas />
---
+++ pages/explore/education-articles/[educationArticleId].jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import PageWrapper from 'components/Layout/PageWrapper';
+import { getEducationArticle } from 'common-util/api';
+import Meta from 'components/Meta';
+import { Spinner } from 'components/Spinner';
+import Markdown from 'common-util/Markdown';
+import { TEXT, TITLE } from 'styles/globals';
+
+const EducationArticle = () => {
+  const router = useRouter();
+  const { educationArticleId } = router.query;
+  const [educationArticle, setEducationArticle] = useState(null);
+
+  useEffect(() => {
+    if (educationArticleId) {
+      getEducationArticle(educationArticleId).then(setEducationArticle);
+    }
+  }, [educationArticleId]);
+
+  if (!educationArticle) return <Spinner />;
+
+  const { title, body: content, headerImage } = educationArticle.attributes;
+  const imagePath = headerImage?.data?.[0]?.attributes?.formats?.large?.url;
+  const imageUrl = `${process.env.NEXT_PUBLIC_API_URL}${imagePath}`;
+
+  return (
+    <PageWrapper>
+      <Meta pageTitle={title} siteImageUrl={imageUrl} />
+      <div className="max-w-3xl mx-auto p-4">
+        <div className={`${TITLE.SMALL} mb-4`}>{title}</div>
+        <Markdown className={TEXT}>{content}</Markdown>
+      </div>
+    </PageWrapper>
+  );
+};
+
+export default EducationArticle;
+
+export async function getServerSideProps({ params }) {
+  return {
+    props: {
+      educationArticleId: params.educationArticleId,
+    },
+  };
+}
