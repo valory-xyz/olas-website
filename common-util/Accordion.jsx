13aaa4644a363998bf72b6134dc20686d714ff73
mohandast52 <mohandast52@gmail.com>
2024-05-10 06:13:41 +0530

    more accordion

---
+++ common-util/Accordion.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+export const Accordion = ({ label, children }) => {
+  const [accordionOpen, setAccordionOpen] = useState(false);
+
+  useEffect(() => {
+    setAccordionOpen(false);
+  }, []);
+
+  return (
+    <div className="py-2">
+      <h2>
+        <button
+          type="button"
+          className="flex items-center justify-between w-full text-left font-semibold py-2"
+          onClick={(e) => {
+            e.preventDefault();
+            setAccordionOpen(!accordionOpen);
+          }}
+          aria-expanded={accordionOpen}
+          aria-controls="accordion-text-01"
+        >
+          <span>{label}</span>
+          <svg
+            className="fill-indigo-500 shrink-0 ml-8"
+            width="16"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              y="7"
+              width="16"
+              height="2"
+              rx="1"
+              className={`transform origin-center transition duration-200 ease-out ${
+                accordionOpen && '!rotate-180'
+              }`}
+            />
+            <rect
+              y="7"
+              width="16"
+              height="2"
+              rx="1"
+              className={`transform origin-center rotate-90 transition duration-200 ease-out ${
+                accordionOpen && '!rotate-180'
+              }`}
+            />
+          </svg>
+        </button>
+      </h2>
+      <div
+        id="accordion-text-01"
+        role="region"
+        aria-labelledby="accordion-title-01"
+        className={`grid text-sm text-slate-600 overflow-hidden transition-all duration-300 ease-in-out ${
+          accordionOpen
+            ? 'grid-rows-[1fr] opacity-100'
+            : 'grid-rows-[0fr] opacity-0'
+        }`}
+      >
+        <div className="overflow-hidden">
+          <div className="pb-3">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Accordion.propTypes = {
+  label: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
---
+++ common-util/Accordion2.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { ChevronDown } from 'lucide-react';
+
+const transition = 'transition-all duration-300 ease-in-out';
+
+export const Accordion = ({ label, children }) => {
+  const [accordionOpen, setAccordionOpen] = useState(true);
+
+  return (
+    <div className="py-2">
+      <button
+        type="button"
+        onClick={() => setAccordionOpen(!accordionOpen)}
+        className={`flex items-center justify-between w-full px-6 py-4 font-medium text-gray-900 bg-gray-100 border border-gray-200 hover:bg-gray-100 ${
+          accordionOpen ? 'rounded-t-xl border-b-0' : 'rounded-xl '
+        }`}
+        aria-expanded="true"
+      >
+        <span>{label}</span>
+        <div>
+          <ChevronDown
+            className={`transform origin-center transition duration-100 ease-out ${
+              accordionOpen && '!rotate-180'
+            }`}
+          />
+        </div>
+      </button>
+
+      <div
+        className={`grid px-6  border border-gray-200 overflow-hidden text-slate-600 rounded-xl rounded-t-none ${transition} ${
+          accordionOpen
+            ? 'grid-rows-[1fr] opacity-100 py-4'
+            : 'grid-rows-[0fr] opacity-0'
+        }`}
+      >
+        <div className="overflow-hidden">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+Accordion.propTypes = {
+  label: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
