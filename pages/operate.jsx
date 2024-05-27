7d1f0d3b5c8948effb5dea9913dd391b6cf48648
mohandast52 <mohandast52@gmail.com>
2024-05-28 01:24:58 +0530

    add metatag for first-reward

--- pages/operate.jsx
+++ pages/operate.jsx
@@ -1,12 +1,27 @@
 import Operate from 'components/OperatePage';
 import PageWrapper from 'components/Layout/PageWrapper';
 import Meta from 'components/Meta';
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
 
-const OperatePage = () => (
-  <PageWrapper>
-    <Meta pageTitle="Olas Operate" />
-    <Operate />
-  </PageWrapper>
-);
+const firstRewardImageSrc = '/images/operate-page/pear-first-reward.png';
+
+const OperatePage = () => {
+  const router = useRouter();
+  const rewardType = router.query?.pearl;
+  const image = useMemo(() => {
+    if (rewardType === 'first-reward') {
+      return firstRewardImageSrc;
+    }
+    return undefined;
+  }, [rewardType]);
+
+  return (
+    <PageWrapper>
+      <Meta pageTitle="Olas Operate" siteImageUrl={image} />
+      <Operate />
+    </PageWrapper>
+  );
+};
 
 export default OperatePage;
