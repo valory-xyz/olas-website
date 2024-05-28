16cd5403c12ff5d729091c8299e5ee00a16a4a62
mohandast52 <mohandast52@gmail.com>
2024-05-28 15:32:16 +0530

    refactor: Update OperatePage to dynamically display reward image based on query parameter

--- pages/operate.jsx
+++ pages/operate.jsx
@@ -1,20 +1,15 @@
+import { useRouter } from 'next/router';
+
 import Operate from 'components/OperatePage';
 import PageWrapper from 'components/Layout/PageWrapper';
 import Meta from 'components/Meta';
-import { useRouter } from 'next/router';
-import { useMemo } from 'react';
 
 const firstRewardImageSrc = '/images/operate-page/pear-first-reward.png';
 
 const OperatePage = () => {
   const router = useRouter();
   const rewardType = router.query?.pearl;
-  const image = useMemo(() => {
-    if (rewardType === 'first-reward') {
-      return firstRewardImageSrc;
-    }
-    return undefined;
-  }, [rewardType]);
+  const image = rewardType === 'first-reward' ? firstRewardImageSrc : undefined;
 
   return (
     <PageWrapper>
