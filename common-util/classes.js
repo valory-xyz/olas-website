f9c2ee91cd22349498dff0770151f67d1c3d9ea5
mohandast52 <mohandast52@gmail.com>
2024-06-04 16:14:15 +0530

    refactor: Update CreateAnAutonomousAiAgent component to import SUB_HEADER_CLASS from common-util/classes

---
+++ common-util/classes.js
@@ -0,0 +1,1 @@
+export const SUB_HEADER_CLASS = 'tracking-tight font-extrabold text-3xl md:font-bold lg:text-4xl';
--- components/LaunchPage/CreateAnAutonomousAiAgent.jsx
+++ components/LaunchPage/CreateAnAutonomousAiAgent.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import SectionWrapper from 'components/Layout/SectionWrapper';
 import Link from 'next/link';
-import SectionHeading from '../SectionHeading';
+import { SUB_HEADER_CLASS } from 'common-util/classes';
 
 const list = [
   'Involves being specific about the agents’ goals and defining matching KPIs that capture these goals',
@@ -12,6 +12,7 @@ const list = [
     <Link
       href="https://github.com/valory-xyz/autonolas-staking-programmes/blob/main/contracts/mech_usage/MechActivityChecker.sol"
       className="text-purple-600"
+      target="_blank"
     >
       Example here
     </Link>
@@ -24,26 +25,37 @@ const list = [
 
 export const CreateAnAutonomousAiAgent = () => (
   <SectionWrapper customClasses="lg:p-24 px-4 py-12 ">
-    <SectionHeading>Network Role</SectionHeading>
-    <Image
-      className="mx-auto mb-24"
-      alt="OLAS Utility"
-      src="/images/launch-page/how-predict-works.svg"
-      width="600"
-      height="474"
-    />
+    <div className="max-w-[800px] mx-auto flex flex-col gap-5">
+      <h2 className={`${SUB_HEADER_CLASS} text-left mb-8`}>
+        How to create an autonomous AI agent economy on Olas
+      </h2>
 
-    {list.map((title, index) => (
-      <div
-        key={index}
-        className="flex flex-col gap-3 bg-gradient-to-r p-4 lg:p-6"
-      >
-        <div className="flex items-center">
-          <h2 className="text-xl">{title}</h2>
-        </div>
+      <p className="mb-4">
+        A simple flow to your own autonomous AI agent economy:
+      </p>
 
-        <p className="text-base">{title}</p>
-      </div>
-    ))}
+      <Image
+        className="mx-auto mb-24"
+        alt="OLAS Utility"
+        src="/images/launch-page/how-predict-works.svg"
+        width="600"
+        height="465"
+      />
+
+      <ol className="list-decimal list-inside">
+        {list.map((title, index) => (
+          <li key={index}>{title}</li>
+        ))}
+      </ol>
+
+      <p className="mt-2">
+        More on staking
+        {' '}
+        <Link href="https://staking.olas.network/" className="text-purple-600">
+          here
+        </Link>
+        .
+      </p>
+    </div>
   </SectionWrapper>
 );
