52548f9af165affac25492606e2f3053c7f5d496
mohandast52 <mohandast52@gmail.com>
2024-06-04 17:16:59 +0530

    refactor: Update protocol page to use SectionWrapper and Link component for improved structure and navigation

--- pages/protocol.jsx
+++ pages/protocol.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { DOCS_BASE_URL } from 'common-util/constants';
 import PageWrapper from 'components/Layout/PageWrapper';
-import { TITLE } from 'styles/globals';
+import SectionWrapper from 'components/Layout/SectionWrapper';
+import { P_MEDIUM_CLASS, SUB_HEADER_CLASS } from 'common-util/classes';
+import Link from 'next/link';
 
 const ProtocolPage = () => (
   <PageWrapper>
-    <h1 className={TITLE.BIG}>Olas Protocol</h1>
-    <a href={`${DOCS_BASE_URL}/protocol`}>Go to Documentation</a>
+    <SectionWrapper customClasses="px-4 py-12 border-b">
+      <div className="max-w-[800px] mx-auto flex flex-col gap-5">
+        <h2 className={`${SUB_HEADER_CLASS} text-left mb-8`}>Olas Protocol</h2>
+
+        <p className={`${P_MEDIUM_CLASS} mb-4`}>
+          <Link href={`${DOCS_BASE_URL}/protocol`} className="text-purple-600">
+            Go to Documentation
+          </Link>
+        </p>
+      </div>
+    </SectionWrapper>
   </PageWrapper>
 );
 
