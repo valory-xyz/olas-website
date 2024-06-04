de42e4490109e01799b4880ed1c51bc493e8d6b9
mohandast52 <mohandast52@gmail.com>
2024-06-04 17:59:24 +0530

    refactor: Update protocol and stack pages to remove unnecessary flex gap and improve structure

--- pages/stack.jsx
+++ pages/stack.jsx
@@ -8,7 +8,7 @@ import Link from 'next/link';
 const StackPage = () => (
   <PageWrapper>
     <SectionWrapper customClasses="px-4 py-12 border-b">
-      <div className="max-w-[800px] mx-auto flex flex-col gap-5">
+      <div className="max-w-[800px] mx-auto flex flex-col">
         <h2 className={`${SUB_HEADER_CLASS} text-left mb-8`}>Olas Stack</h2>
 
         <p className={`${P_MEDIUM_CLASS} mb-4`}>
