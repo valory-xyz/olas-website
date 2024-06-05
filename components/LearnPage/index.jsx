7ffe7e85e40fbb60573ec2d0ae136b84a912bd3e
mohandast52 <mohandast52@gmail.com>
2024-06-05 16:17:03 +0530

    refactor: Remove unnecessary code and improve structure of LearnPage component

--- components/LearnPage/index.jsx
+++ components/LearnPage/index.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import Link from 'next/link';
-import SectionWrapper from 'components/Layout/SectionWrapper';
 import WhatIsOlas from './2WhatIsOlas';
 import DecentralizedAndSophisticated from './3DecentralizedAndSophisticated';
 import HowDoOlasWork from './4HowDoOlasWork';
@@ -24,20 +21,10 @@ export const LEARN_LIST = [
 
 export const LearnPage = () => (
   <PageWrapper>
-    <Meta pageTitle="Learn" description="Build your understanding of what Olas is and how it works." />
-    <SectionWrapper customClasses="px-8 mt-12 mb-20 max-w-screen-xl mx-auto">
-      <h1 className="text-4xl font-bold tracking-tighter text-center sm:text-5xl md:text-6xl mb-6">Learn</h1>
-      <span className="block text-2xl text-center mb-8">Jump To:</span>
-      <ul className="flex flex-col gap-4 bg-gray-100 rounded-lg p-6 mx-auto">
-        {LEARN_LIST.map((e) => (
-          <li key={e.id}>
-            <Link href={`/learn/#${e.id}`} className="text-2xl text-purple-600 hover:text-purple-800 visited:text-purple-600">
-              {e.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </SectionWrapper>
+    <Meta
+      pageTitle="Learn"
+      description="Build your understanding of what Olas is and how it works."
+    />
 
     <div className="flex flex-col gap-12 container mx-auto px-4">
       <WhatIsOlas />
