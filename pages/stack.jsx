37fbe6abcfcf92a6c1003fabf6edc77b11b24a54
mohandast52 <mohandast52@gmail.com>
2024-06-07 15:59:37 +0530

    refactor: Update StackPage components and meta tags

--- pages/stack.jsx
+++ pages/stack.jsx
@@ -1,30 +1,21 @@
-import Link from 'next/link';
-
-import { DOCS_BASE_URL } from 'common-util/constants';
-import {
-  TEXT_MEDIUM_CLASS,
-  SCREEN_WIDTH_LG,
-  SUB_HEADER_CLASS,
-} from 'common-util/classes';
 import PageWrapper from 'components/Layout/PageWrapper';
-import SectionWrapper from 'components/Layout/SectionWrapper';
+import Meta from 'components/Meta';
+import { StackHeader } from 'components/StackPage/StackHeader';
+import { StackCta } from 'components/StackPage/StackCta';
+import { StackKeyFeatures } from 'components/StackPage/StackKeyFeatures';
+import { StackFaq } from 'components/StackPage/StackFaq';
 
 const StackPage = () => (
   <PageWrapper>
-    <SectionWrapper customClasses="px-4 py-12 border-b">
-      <div className={SCREEN_WIDTH_LG}>
-        <h2 className={`${SUB_HEADER_CLASS} text-left mb-8`}>Olas Stack</h2>
+    <Meta
+      pageTitle="Stack"
+      description="Build your understanding of what Olas is and how it works."
+    />
 
-        <p className={`${TEXT_MEDIUM_CLASS} mb-4`}>
-          <Link
-            href={`${DOCS_BASE_URL}/open-autonomy`}
-            className="text-purple-600"
-          >
-            Go to Documentation
-          </Link>
-        </p>
-      </div>
-    </SectionWrapper>
+    <StackHeader />
+    <StackKeyFeatures />
+    <StackFaq />
+    <StackCta />
   </PageWrapper>
 );
 
