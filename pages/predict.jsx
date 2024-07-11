6b6ff02ca50d334318d0bab34c5643e16e206b8d
mohandast52 <mohandast52@gmail.com>
2024-07-11 19:03:33 +0530

    feat: Add PredictPage components (init)

---
+++ pages/predict.jsx
@@ -0,0 +1,14 @@
+import Meta from 'components/Meta';
+import PageWrapper from 'components/Layout/PageWrapper';
+import { PredictHeader } from 'components/PredictPage/PredictHeader';
+
+const Predict = () => (
+  <PageWrapper>
+    {/* TODO */}
+    <Meta pageTitle="Predict" description="" />
+
+    <PredictHeader />
+  </PageWrapper>
+);
+
+export default Predict;
