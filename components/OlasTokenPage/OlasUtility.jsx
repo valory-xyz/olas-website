100f6e44524136481d9684307388f93de8650da8
Atatakai <atatakai@Atatakais-MacBook-Pro.local>
2024-07-17 23:48:31 +1000

    Update utility section

--- components/OlasTokenPage/OlasUtility.jsx
+++ components/OlasTokenPage/OlasUtility.jsx
@@ -1,29 +1,28 @@
 import Image from 'next/image';
 import SectionWrapper from 'components/Layout/SectionWrapper';
-import SectionHeading from '../SectionHeading';
 
 function OlasUtility() {
   return (
-    <SectionWrapper customClasses="lg:p-24 px-4 py-12 text-center">
-      <div id="olas-token" />
-      <SectionHeading color="text-purple-950">Utility</SectionHeading>
-      <div className="text-3xl font-light tracking-tight text-gray-600 leading-normal mb-4 lg:w-3/4 mx-auto">
+    <SectionWrapper
+      customClasses="lg:p-24 px-4 py-12 text-center"
+      id="olas-token"
+    >
+      <div className="text-5xl font-bold mb-8 tracking-tight text-black text-center">
+        Utility
+      </div>
+      <p className="text-lg font-light tracking-tight text-gray-600 leading-normal">
         OLAS provides access to the core functions of the network.
-      </div>
-      <div className="text-3xl font-light tracking-tight text-gray-600 leading-normal mb-12 lg:w-3/4 mx-auto">
-        Bond, stake and lock OLAS to contribute to shaping the network.
-      </div>
+      </p>
+      <p className="text-lg font-light tracking-tight text-gray-600 leading-normal mb-12">
+        Code, bond, operate and lock OLAS to contribute to shaping the network.
+      </p>
       <Image
         className="mx-auto mb-24"
         alt="OLAS Utility"
-        src="/images/olas-utility.svg"
-        width="1076"
-        height="474"
+        src="/images/olas-utility.png"
+        width="1312"
+        height="494"
       />
-      <div className="text-3xl font-light tracking-tight text-gray-600 leading-normal mb-24 lg:w-3/4 mx-auto">
-        The OLAS token has voting escrow (ve), Olympus-inspired bonding and
-        staking mechanisms built-in.
-      </div>
     </SectionWrapper>
   );
 }
