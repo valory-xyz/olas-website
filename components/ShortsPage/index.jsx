6a0f84efffebe2dc59d5d7e0c2f16cbbd27b85cd
mohandast52 <mohandast52@gmail.com>
2024-08-05 19:41:05 +0530

    feat: add initial page

---
+++ components/ShortsPage/index.jsx
@@ -0,0 +1,6 @@
+export const ShortsPage = () => (
+  <div className="flex flex-col items-center justify-center h-full">
+    <h1 className="text-5xl font-bold">Shorts</h1>
+    <p className="text-lg mt-4">This page is under construction.</p>
+  </div>
+);
---
+++ pages/shorts.jsx
@@ -0,0 +1,13 @@
+import PageWrapper from 'components/Layout/PageWrapper';
+import Meta from 'components/Meta';
+import { ShortsPage } from 'components/ShortsPage';
+
+const Shorts = () => (
+  <PageWrapper>
+    <Meta pageTitle="Shorts" description="TODO" />
+
+    <ShortsPage />
+  </PageWrapper>
+);
+
+export default Shorts;
