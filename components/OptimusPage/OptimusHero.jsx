c95fb9e9d0541b7fef209f3642db9ae0e37f7e25
atepem <57289722+atepem@users.noreply.github.com>
2024-10-17 13:47:49 +0100

    chore: image icons for optimus and mech

--- components/OptimusPage/OptimusHero.jsx
+++ components/OptimusPage/OptimusHero.jsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import { MAIN_TITLE_CLASS, TEXT_MEDIUM_LIGHT_CLASS } from 'common-util/classes';
-import SectionWrapper from 'components/Layout/SectionWrapper';
+import { MAIN_TITLE_CLASS, TEXT_MEDIUM_LIGHT_CLASS } from "common-util/classes";
+import SectionWrapper from "components/Layout/SectionWrapper";
 
 const LeftContent = () => (
   <div className="lg:col-span-5 lg:col-start-2 lg:p-0 lg:text-left lg:gap-0 lg:items-start md:mb-12 flex-col flex text-center item-center px-5 mb-2 gap-6">
@@ -12,9 +12,9 @@ const LeftContent = () => (
     </div>
 
     <h2 className={MAIN_TITLE_CLASS}>
-      AI Agent-powered
+      AI agent-powered
       <br />
-      DeFi Management
+      DeFi management
     </h2>
   </div>
 );
