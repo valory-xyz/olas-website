f8d485e0003ec8b29eba5561d905141a382b7345
atepem <57289722+atepem@users.noreply.github.com>
2024-10-21 17:38:13 +0100

    chore: move reusables

--- components/OptimusPage/OptimusAgentMetrics.jsx
+++ components/OptimusPage/OptimusAgentMetrics.jsx
@@ -2,9 +2,7 @@ import { SECTION_BOX_CLASS } from 'common-util/classes';
 import SectionWrapper from 'components/Layout/SectionWrapper';
 import { Card } from 'components/ui/card';
 import Image from 'next/image';
-
-const CARD_BG =
-  'border-1.5 border-gray-200 rounded-2xl p-6 bg-gradient-to-t from-[#F2F4F7] to-white';
+import { CARD_BG } from 'styles/globals';
 
 export const OptimusAgentMetrics = () => (
   <SectionWrapper customClasses={`${SECTION_BOX_CLASS} lg:py-14`}>
@@ -12,7 +10,7 @@ export const OptimusAgentMetrics = () => (
       <Card className={`${CARD_BG} max-h-[250px] p-16`}>
         <Image
           alt="Optimus metrics"
-          src="/images/optimus-page/optimus-metrics.svg"
+          src="/images/metrics.svg"
           height={48}
           width={48}
           className="mb-6 mx-auto"
