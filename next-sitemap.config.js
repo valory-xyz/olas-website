07bdb3f0284fca4b2bc66f017bcd5c4ca62904c6
atepem <57289722+atepem@users.noreply.github.com>
2024-11-12 16:50:49 +0000

    chore: sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteUrl: 'https://olas.network',
   generateRobotsTxt: true,
+  sitemapSize: 5000,
   exclude: ['/server-sitemap.xml'], 
   robotsTxtOptions: {
     additionalSitemaps: [
