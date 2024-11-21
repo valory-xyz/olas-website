42b549d552bec21977b30c566316297ddcd65954
atepem <57289722+atepem@users.noreply.github.com>
2024-11-21 15:59:37 +0000

    chore: card fixes for operate metrics

--- components/OperatePage/OperateMetrics.jsx
+++ components/OperatePage/OperateMetrics.jsx
@@ -33,7 +33,7 @@ export const OperateMetrics = () => {
         id: 'operators',
         imageSrc: 'operators.png',
         labelText: 'Operators',
-        subText: 'All-time unique participants',
+        subText: 'All-time unique operators',
         value: metrics?.totalUniqueStakers?.toLocaleString(),
         source: OLAS_ECONOMY_DASHBOARD_URL,
       },
@@ -51,7 +51,7 @@ export const OperateMetrics = () => {
 
   return (
     <SectionWrapper customClasses='mt-16'>
-      <Card className='flex flex-row gap-6 p-8 mx-auto border border-purple-200 rounded-full text-xl w-fit rounded-2xl bg-gradient-to-t from-[#F1DBFF] to-[#FDFAFF] items-center'>
+      <Card className='flex flex-row gap-6 p-6 mx-auto border border-purple-200 rounded-full text-xl w-fit rounded-2xl bg-gradient-to-t from-[#F1DBFF] to-[#FDFAFF] items-center'>
         {data.map((item, index) => {
           let borderClassName = '';
           if (index == 0) borderClassName += 'lg:border-r-1.5 border-purple-200';
@@ -68,9 +68,9 @@ export const OperateMetrics = () => {
 
           return (
             <div key={item.id} className={`text-center w-[345px] py-6 2xl:py-3 px-8 border-gray-300 h-full max-sm:w-full ${borderClassName}`}>
-              <div className='flex gap-2 mb-3 justify-center'>
+              <div className='flex gap-2 mb-5 justify-center'>
                 <Image alt='Operate' src={`/images/operate-page/${item.imageSrc}`} width={35} height={35} />
-                <span className='text-lg text-black'>{item.labelText}</span>
+                <span className='text-lg text-black flex items-center'>{item.labelText}</span>
               </div>
               <span className='block text-5xl max-sm:text-4xl font-extrabold mb-4 text-purple-600'>{getValue()}</span>
               <span className='block text-lg text-slate-700'>{item.subText}</span>
