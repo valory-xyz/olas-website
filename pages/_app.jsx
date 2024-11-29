4f2c8422430bfad433e7a5d5ffcdbf82485adbb3
atepem <57289722+atepem@users.noreply.github.com>
2024-11-29 10:11:44 +0000

    chore: lint fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,7 +4,7 @@ import 'styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <PlausibleProvider domain='olas.network' trackLocalhost={true}>
+    <PlausibleProvider domain="olas.network" trackLocalhost={true}>
       <Component {...pageProps} />
     </PlausibleProvider>
   );
