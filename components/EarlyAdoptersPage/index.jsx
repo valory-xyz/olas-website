9aff56d3716e5c283897f4ab0877bb0c8177b1d8
Atatakai <densatsu@mail.ru>
2025-01-09 19:55:34 +0400

    fix: descriptions

--- components/EarlyAdoptersPage/index.jsx
+++ components/EarlyAdoptersPage/index.jsx
@@ -156,6 +156,11 @@ DownloadLinks.propTypes = {
   agentType: PropTypes.string.isRequired,
 };
 
+const DESCRIPTIONS = {
+  agentsfun: 'Get the latest version of Pearl with Agents.fun support',
+  modius: 'Get the latest version of Pearl with Modius support',
+};
+
 export const EarlyAdoptersPage = ({ agentType }) => (
   <SectionWrapper customClasses={`border ${SECTION_BOX_CLASS}`}>
     <div className="max-w-[500px] mx-auto flex flex-col items-center text-center mb-16">
@@ -167,9 +172,7 @@ export const EarlyAdoptersPage = ({ agentType }) => (
         className="mb-12"
       />
       <span className={TEXT_MEDIUM_LIGHT_CLASS}>PEARL FOR EARLY ADOPTERS</span>
-      <h2 className={`${SUB_HEADER_CLASS} mt-2`}>
-        Get the latest version of Pearl with Agents.fun support
-      </h2>
+      <h2 className={`${SUB_HEADER_CLASS} mt-2`}>{DESCRIPTIONS[agentType]}</h2>
     </div>
     <DownloadLinks agentType={agentType} />
   </SectionWrapper>
