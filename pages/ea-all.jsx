4f61d9fba84a90e14d0b13e86091060f018ae22b
Atatakai <densatsu@mail.ru>
2025-02-03 20:49:21 +0400

    feat: add ea page supporting all 3 agents

---
+++ pages/ea-all.jsx
@@ -0,0 +1,12 @@
+import EarlyAdoptersPage from 'components/EarlyAdoptersPage';
+import PageWrapper from 'components/Layout/PageWrapper';
+
+const EaModiusPage = () => {
+  return (
+    <PageWrapper>
+      <EarlyAdoptersPage agentType="all" />
+    </PageWrapper>
+  );
+};
+
+export default EaModiusPage;
