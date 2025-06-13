5ab0f76ae487783c13504895c3733897f94a8948
atepem <57289722+atepem@users.noreply.github.com>
2025-06-13 20:17:14 +0100

    style: more questions

--- components/PearlPage/MeetPearl.jsx
+++ components/PearlPage/MeetPearl.jsx
@@ -147,11 +147,11 @@ export const MeetPearlContent = () => (
       </h2>
 
       <p className={TEXT_CLASS}>
-        Pearl brings you the ultimate collection of AI agents in one app. From
-        asset managers to custom AI influencers, Pearl has it all. Choose from a
-        growing range of Olas agents, stake OLAS, and let them work autonomously
-        — earning you potential rewards from your AI agents&apos; work and Olas
-        Staking.
+        Pearl brings you the ultimate collection of AI agents in one app — your
+        Agent App Store. From asset managers to custom AI influencers, Pearl has
+        it all. Choose from a growing range of Olas agents, stake OLAS, and let
+        them work autonomously — earning you potential rewards from your crypto
+        agents&apos; work and Olas Staking.
       </p>
     </div>
     <Image
