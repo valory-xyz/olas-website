c7008c66d666c107d24bbb9c057c9374d2437dce
Atatakai <densatsu@mail.ru>
2025-06-17 17:43:43 +0400

    feat: hero and trusted by sections (#289)
    * style: hero page and trusted by
    
    * fix: video is not showing and hovering issue with trusted by items
    
    * feat: display tooltip with quote
    
    * feat: update mobile view
    
    * fix: review fixes
    
    * chore: fix types
    
    ---------
    
    Co-authored-by: atepem <57289722+atepem@users.noreply.github.com>

---
+++ components/HomepageSection/TrustedBy.jsx
@@ -0,0 +1,29 @@
+import { SUB_HEADER_CLASS } from 'common-util/classes';
+import SectionWrapper from 'components/Layout/SectionWrapper';
+import { Button } from 'components/ui/button';
+import quotes from 'data/trustedBy.json';
+import Link from 'next/link';
+import { Trustee } from './Trustee';
+
+export const TrustedBy = () => (
+  <SectionWrapper
+    id="social-proof"
+    customClasses="lg:p-24 px-4 py-20 mt-20"
+    backgroundType="NONE"
+  >
+    <h2 className={`${SUB_HEADER_CLASS} text-center mb-6 lg:mb-14`}>
+      Trusted by Leading Web3 Teams and Users
+    </h2>
+
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-4xl mx-auto mb-10 md:mb-14">
+      {quotes.map((quote, index) => (
+        <Trustee key={index} quote={quote} />
+      ))}
+    </div>
+    <div className="flex justify-center">
+      <Button variant="outline" size="lg" asChild className="">
+        <Link href="/case-studies">Explore Case Studies</Link>
+      </Button>
+    </div>
+  </SectionWrapper>
+);
