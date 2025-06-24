508eba9cc6f4bca1ab4a66de4024bec34d8e1e43
Atatakai <densatsu@mail.ru>
2025-06-24 14:28:21 +0400

    chore: move founding members to data folder

--- components/AboutPage/BubbleRing.jsx
+++ components/AboutPage/BubbleRing.jsx
@@ -1,36 +1,5 @@
+import initialBubbles from 'data/founding-members.json';
 import Image from 'next/image';
-
-const initialBubbles = [
-  { icon: '0x61726469616e.png', link: 'https://x.com/0x61726469616e' },
-  { icon: '8baller11.png', link: 'https://x.com/8baller11' },
-  { icon: 'angrybayblade.png', link: 'https://x.com/angrybayblade' },
-  { icon: 'anon.png', link: 'https://x.com/keeevin' },
-  { icon: 'bradrian_0x.png', link: 'https://x.com/bradrian_0x' },
-  { icon: 'brahmafi.png', link: 'https://x.com/BrahmaFi' },
-  { icon: 'bubble.png', link: 'https://x.com/MLescrauwaet' },
-  { icon: 'contentwillvary.png', link: 'https://x.com/contentwillvary' },
-  { icon: 'david_enim.png', link: 'https://x.com/david_enim' },
-  { icon: 'dvilelaf.png', link: 'https://x.com/dvilelaf' },
-  { icon: 'federiconitidi.png', link: 'https://x.com/federiconitidi' },
-  { icon: 'jose.png', link: null },
-  { icon: 'kupermind.png', link: 'https://x.com/kupermind' },
-  { icon: 'manos.png', link: null },
-  { icon: 'mltech.png', link: 'https://x.com/MLTech' },
-  { icon: 'mohan_das_.png', link: 'https://x.com/Mohan_Das_' },
-  { icon: 'mralbertchen.png', link: 'https://x.com/mralbertchen' },
-  { icon: 'nonstopTheo.png', link: 'https://x.com/nonstopTheo' },
-  { icon: 'philism_.png', link: 'https://x.com/philism_' },
-  { icon: 'postmint_xyz.png', link: 'https://x.com/postmint_xyz' },
-  { icon: 'proofgp.png', link: 'https://x.com/proofgp' },
-  { icon: 'semanticvc.png', link: 'https://x.com/semanticvc' },
-  { icon: 'signature_vc.png', link: 'https://x.com/signature_vc' },
-  { icon: 'tannedoaksprout.png', link: 'https://x.com/tannedoaksprout' },
-  { icon: 'thomasmaybrier.png', link: 'https://x.com/thomasmaybrier' },
-  { icon: 'trueventures.png', link: 'https://x.com/trueventures' },
-  { icon: 'twipia6.png', link: 'https://x.com/twiPia6' },
-  { icon: 'valorianxyz.png', link: 'https://x.com/valorianxyz' },
-  { icon: 'x10xalex.png', link: 'https://x.com/x10xalex' },
-];
 
 const layout = {
   top: [6, 8],
