df486cd723a87f74e0cb5d3804206a30ee70c7b9
Atatakai <densatsu@mail.ru>
2025-07-17 09:52:27 +0400

    fix: add caching for dune queries

---
+++ pages/api/dune.js
@@ -0,0 +1,41 @@
+const CACHE_DURATION_SECONDS = 12 * 60 * 60; // 12 hours
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  const { queryId } = req.query;
+
+  if (!queryId) {
+    return res.status(400).json({ error: 'Missing queryId parameter' });
+  }
+
+  res.setHeader(
+    'Vercel-CDN-Cache-Control',
+    `max-age=${CACHE_DURATION_SECONDS}`,
+  );
+  res.setHeader('CDN-Cache-Control', `max-age=${CACHE_DURATION_SECONDS}`);
+  res.setHeader(
+    'Cache-Control',
+    `public, max-age=${CACHE_DURATION_SECONDS}, stale-while-revalidate=${CACHE_DURATION_SECONDS * 2}`,
+  );
+
+  try {
+    const response = await fetch(
+      `https://api.dune.com/api/v1/query/${queryId}/results`,
+      {
+        headers: {
+          'X-Dune-API-Key': process.env.NEXT_PUBLIC_DUNE_API_KEY,
+        },
+      },
+    );
+
+    const result = await response.json();
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error in handler:', error);
+    return res.status(500).json({ error: 'Failed to fetch dune query.' });
+  }
+}
