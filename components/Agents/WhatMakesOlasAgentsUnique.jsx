29da9e0e70a61ca6a018d71d064d1ca4dd0d3710
atepem <57289722+atepem@users.noreply.github.com>
2025-07-21 15:16:36 +0100

    style: agents section redone for working links

--- components/Agents/WhatMakesOlasAgentsUnique.jsx
+++ components/Agents/WhatMakesOlasAgentsUnique.jsx
@@ -1,5 +1,7 @@
+import { REGISTRY_URL } from 'common-util/constants';
 import SectionWrapper from 'components/Layout/SectionWrapper';
 import SectionHeading from 'components/SectionHeading';
+import { ExternalLink, Link, SubsiteLink } from 'components/ui/typography';
 import Image from 'next/image';
 
 export const WhatMakesOlasAgentsUnique = () => (
@@ -10,12 +12,85 @@ export const WhatMakesOlasAgentsUnique = () => (
         Olas agents are autonomous AI agents that satisfy these additional
         characteristics:
       </p>
-      <Image
-        src="/images/agents/unique-agents.png"
-        alt="What Makes Olas' AI Agents Unique?"
-        width={1320}
-        height={1086}
-      />
+      <div className="text-left mx-auto relative">
+        <div className="max-md:hidden">
+          <div className="agents-bg" />
+        </div>
+        <div className="flex flex-col md:flex-row gap-4 lg:gap-14 md:pt-14 relative z-10">
+          <div className="md:hidden">
+            <Image
+              src="/images/agents/olas-agent.png"
+              alt="Olas AI Agents"
+              width={157}
+              height={157}
+              className="mx-auto"
+            />
+            <p className="-top-[40px] relative text-center">Olas AI Agent</p>
+          </div>
+          <div className="flex flex-col justify-between max-w-[267px]">
+            <div>
+              <Image
+                src="/images/agents/safe.png"
+                alt="Safe"
+                width={267}
+                height={198}
+              />
+              <p className="-top-[35px] relative">
+                They self-custody resources via a wallet. Olas agents use
+                account abstraction (via{' '}
+                <ExternalLink href="https://safe.global/">Safe</ExternalLink>).
+              </p>
+            </div>
+            <div>
+              <Image
+                src="/images/agents/poaa.png"
+                alt="PoAA"
+                width={267}
+                height={112}
+              />
+              <p className="-top-[30px] relative">
+                They optionally earn rewards via{' '}
+                <Link href="/staking">Olas Staking</Link>.
+              </p>
+            </div>
+          </div>
+          <div className="content-center max-md:hidden">
+            <Image
+              src="/images/agents/olas-agent.png"
+              alt="Olas AI Agents"
+              width={157}
+              height={157}
+            />
+            <p className="-top-[40px] relative text-center">Olas AI Agent</p>
+          </div>
+          <div className="flex flex-col justify-between max-w-[267px]">
+            <div>
+              <Image
+                src="/images/agents/registry.png"
+                alt="Registry"
+                width={267}
+                height={276}
+              />
+              <p className="-top-[35px] relative">
+                They are listed on a global agent directory called{' '}
+                <SubsiteLink href={REGISTRY_URL}>Olas Registry</SubsiteLink>.
+              </p>
+            </div>
+            <div>
+              <Image
+                src="/images/agents/pearl.png"
+                alt="Pearl"
+                width={267}
+                height={300}
+              />
+              <p className="-top-[30px] relative">
+                They optionally are made accessible to end-users via{' '}
+                <Link href="/pearl">Pearl, the AI Agent App Store</Link>.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </SectionWrapper>
 );
