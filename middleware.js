02ffff2bd24f0cebc85415a4d083a59ad07a8ed6
atepem <57289722+atepem@users.noreply.github.com>
2025-12-05 23:05:35 +0000

    feat: block restricted regions

--- middleware.js
+++ middleware.js
@@ -5,19 +5,15 @@ const BLOCKED_REGIONS = ['UA-14', 'UA-09', 'UA-65', 'UA-23', 'GB-LND']; //GB-LDN
 export function middleware(request) {
   const region = request.headers.get('x-vercel-ip-country-region');
 
+  if (
+    request.nextUrl.pathname === '/restricted' ||
+    request.nextUrl.pathname === '/disclaimer'
+  ) {
+    return NextResponse.next();
+  }
+
   if (region && BLOCKED_REGIONS.includes(region)) {
-    return new NextResponse(
-      JSON.stringify({
-        error: 'Access denied',
-        message: 'This service is not available in your region.',
-      }),
-      {
-        status: 403,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
+    return NextResponse.redirect(new URL('/restricted', request.url));
   }
 
   return NextResponse.next();
