6a0707e6ddb12f20f4eb5cb2936860d30946165a
Rajat Verma <rajatverma5885045@gmail.com>
2026-01-09 01:41:26 +0530

    fix: lint

--- pages/api/refresh-metrics/main.ts
+++ pages/api/refresh-metrics/main.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
